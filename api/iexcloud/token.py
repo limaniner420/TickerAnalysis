d5f42c6f75bd9365769eb472ebb4f85624c82b4c
Lima <56560333+limaniner420@users.noreply.github.com>
2022-03-24 13:06:50 -0700

    Can now select tokens according to call mode.

--- api/iexcloud/token.py
+++ api/iexcloud/token.py
@@ -3,10 +3,10 @@ import os
 
 # Reads the config.cfg at root of project for token.
 
-def get_token():
+def get_token(mode: str = "sandbox"):
     """ 
     Reads token from config.cfg at root directory of project.
     """
     config = configparser.ConfigParser()
     config.read_file(open(os.path.join(os.getcwd(), "config.cfg")))
-    return config.get('token', 'token_IEX')
+    return config.get('token', 'token_IEX_sandbox') if mode == "sandbox" else config.get('token', 'token_IEX_prod')
--- api/iexcloud/url.py
+++ api/iexcloud/url.py
@@ -3,14 +3,13 @@ from . import token
 def get_URLbase(mode: str = "sandbox", ver: str = "stable"):
     return "https://" + mode + ".iexapis.com/" + ver
 
-def get_URLtoken():
-    return "?token=" + token.get_token()
+def get_URLtoken(mode: str = "sandbox"):
+    return "?token=" + token.get_token(mode)
 
 def compose_URL(mid: str = "", mode: str = "sandbox", ver: str = "stable"):
     """ 
-    :ticker: Ticker symbol of listed securities on NA exchanges.
-    :period: Desired range of data.
-    :mode: Specify whether to access sandbox ("sandbox") data or real ("cloud") data. Real data may incur charges. Default sandbox.
-    :ver: Specify api version. Default stable.
+    mid: Endpoint with parameters.
+    mode: Specify whether to access sandbox ("sandbox") data or real ("cloud") data. Real data may incur charges. Default sandbox.
+    ver: Specify api version. Default stable.
     """
-    return get_URLbase(mode, ver) + mid + get_URLtoken()
+    return get_URLbase(mode, ver) + mid + get_URLtoken(mode)
