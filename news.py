e3d5a1e7d34320e153c325d1193d98e0af3abd71
junpin-foo <foojunpin@gmail.com>
2022-03-25 21:59:38 -0700

    added news function in data

---
+++ news.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import json
+from metrics import metrics 
+from api.iexcloud import data
+
+j = data.news("AAPL", "10")
+
+with open('data.json', 'w') as f:
+    json.dump(j, f)
+
+hist = pd.json_normalize(j)
+print(hist)
