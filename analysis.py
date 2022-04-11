8d492232218bb30d363e749e9c40d7e36fae0621
lawrence900613 <lawrence900613@gmail.com>
2022-04-10 23:03:43 -0700

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -5,12 +5,20 @@ import metrics.regression as reg
 import json
 import pandas as pd
 import re
+from datetime import datetime
+import matplotlib.pyplot as plt
 
     
 word = input("Please input a stock symbol which you want to analysis :")
-file = pd.read_json("data.json")
-data = pd.DataFrame(file[word]['chart'])
-weight = 0
+
+try:
+    file = pd.read_json("data.json")
+    data = pd.DataFrame(file[word]['chart'])    
+    weight = 0
+except Exception as e:
+    print("the stock data haven't fetched, please fetch first in the fetch.py")
+    exit()
+
 price = data.iloc[-1]['close']
 
 #bolli
@@ -27,7 +35,6 @@ elif (price >= d2["+1sd"]):
 else:
     weight -= 1
 
-print(weight)
 
 #macd
 macd = metum.macd(data)
@@ -39,28 +46,51 @@ elif(mval > 0 ):
 else:
     weight -= 0.5
 
-print(weight)
-
-
+print(macd)
 
 #rsi
 rsi = metum.rsi(data)
 mean = rsi['RSI'].rolling(len(rsi)).mean().iloc[-1]
-if(rsi.iloc[-1]['RSI'] == mean):
+if(rsi.iloc[-1]['RSI'] == mean or (rsi.iloc[-1]['RSI']>= 30 and rsi.iloc[-1]['RSI'] <= 70)):
     weight += 0
-elif(rsi.iloc[-1]['RSI'] >= mean):
+elif(rsi.iloc[-1]['RSI'] <= mean or rsi.iloc[-1]['RSI'] < 30):
     weight += 0.5
 else:
     weight -= 0.5
 
 
-print(weight)
 
-#volatility
-#volat = metum.volatility(data)
-#print(volat)
+plt.show()
+
 
 #Stochastic
 Stoc = metum.Stochastic(data)
-#print(Stoc)
+if(Stoc.iloc[-1]['K'] > 80 and Stoc.iloc[-1]['d'] > 80 and Stoc.iloc[-1]['d'] > Stoc.iloc[-1]['k']):
+    weight -= 1
+elif(Stoc.iloc[-1]['K'] < 20 and Stoc.iloc[-1]['d'] < 20 and Stoc.iloc[-1]['k'] > Stoc.iloc[-1]['d']):
+    weight += 1
 
+
+#graphing 
+fig, (price, Rsi,kd) = plt.subplots(3)
+fig.suptitle(word)
+price_data = data[['date','close']]
+price_data = price_data.set_index("date").dropna()
+price_data['close'].plot(ax=price)
+price.set_ylabel('Price')
+rsi['RSI'].plot(ax=Rsi)
+Rsi.set_ylim(0,100)
+Rsi.axhline(30, color='r', linestyle='--')
+Rsi.axhline(70, color='r', linestyle='--')
+Rsi.set_ylabel('RSI')
+Stoc[['K', 'D']].plot(ax=kd)
+kd.set_ylim(0,100)
+kd.axhline(20, linestyle='--', color="r")
+kd.axhline(80, linestyle="--", color="r")
+plt.show()
+
+
+if(weight > 0):
+    print("After different analysis, it is a good timing to buy this stock if you are considering when to buy it")
+else:
+    print("After different analysis, it is not a good timing to buy this stock, you might consider to sell or hold it")
