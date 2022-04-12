05c6b5311fc4ed1c802241e1615030b4992e834b
lawrence900613 <lawrence900613@gmail.com>
2022-04-11 19:19:37 -0700

    Update Fetch.py

--- Fetch.py
+++ Fetch.py
@@ -1,7 +1,6 @@
 import api.iexcloud.data as api
 import metrics.metrics as met
 import metrics.momentum as metum
-import metrics.regression as reg
 import json
 import pandas as pd
 import re
@@ -12,19 +11,12 @@ pattern = re.compile(r'\s+')
 word = input("Please input a list stock symbol, if there are multiple, please seperate it with comma :")
 word = re.sub(pattern, '', word)
 word=  word.split(",")
-
 time = input("Plase input the time range: ")
 
-type = input("Please input a list of type you want, if there are multiple, please seperate it with comma :")
-type = re.sub(pattern, '', type)
-type=  type.split(",")
-
 try:
-    j = api.batch(word,type,time)
+    j = api.batch(word,["chart","news"],time)
     with open('data.json', 'w') as f:
      json.dump(j, f)
-    j = api.batch(word,type,time)
-
 except Exception as e:
     print(e)
 
